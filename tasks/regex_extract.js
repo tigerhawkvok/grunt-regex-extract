9e961d823ccac5900600d00440b7395fd04087d6
Philip Kahn <tigerhawkvok@gmail.com>
2016-07-11 11:01:38 -0700

    Update regex_extract.js

--- tasks/regex_extract.js
+++ tasks/regex_extract.js
@@ -4,6 +4,9 @@
  *
  * Copyright (c) 2014 Miren Pau
  * Licensed under the MIT license.
+ *
+ * 
+ * Forked and updated by Philip Kahn, July 2016
  */
 
 module.exports = function (grunt) {
@@ -14,70 +17,62 @@ module.exports = function (grunt) {
   
 var options = this.options(
     {
-      regex : "(.*|\n*)",
-	  modifiers: "ig",
-      matchPoints : "1",
-	  includePath : true
+        regex : "(.*|\n*)",
+	modifiers: "ig",
+        matchPoints : "1",
+	includePath : true,
+        printMatches: false
     });
-
+     
     // For each [destination : source map] in list
     this.files.forEach(function(file)
     {
-
-      // For each source file
-      var src = file.src.filter(function(filepath)
-      {
-
-        // Let me know through warning if a source doesn't exist
-        if (!grunt.file.exists(filepath))
-        {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        }
-        else
-        {
-          return true;
-        }
-      }).map(function(filepath)
-      {
-        // Read file source.
-        var filestring = grunt.file.read(filepath);
-        var re = new RegExp(options.regex, options.modifiers);
-        var match = "";
-        var matches = "";
-        var matchPointsArray = options.matchPoints.split(",");
-
-        while ((match = re.exec(filestring)) !== null)
-        {
-          var matchstring = "";
-		  if(options.includePath)
-		  {
-			matchstring += filepath;
-		  }
-		  
-		  matchPointsArray.forEach(function(current)
-          {
-			if(!options.includePath && matchstring.length === 0)
-			{
-				matchstring += match[current].trim();
-			}
-			else
-			{
-				matchstring += ("," + match[current].trim());			
-			}
-          });
-          matches = matches + matchstring + "\n";
-          matchstring = undefined;
-        }
-		grunt.log.writeln(matches);
-        return matches;
+        var countMatches = 0;
+        // For each source file
+        var src = file.src.filter(function(filepath) {            
+            // Let me know through warning if a source doesn't exist
+            if (!grunt.file.exists(filepath)) {
+                grunt.log.warn('Source file "' + filepath + '" not found.');
+                return false;
+            } else {
+                return true;
+            }
+        }).map(function(filepath) {
+          // Read file source.
+          var filestring = grunt.file.read(filepath);
+          var re = new RegExp(options.regex, options.modifiers);
+          var matches = "";
+          var matchPointsArray = options.matchPoints.split(",");
+          var match = re.exec(filestring);
+          while (match !== null) {
+              countMatches++;
+              var matchstring = match[0];
+	      if(options.includePath) {
+		  matchstring += filepath;
+	      }
+	      
+	      matchPointsArray.forEach(function(current) {
+                  if(match[current] != undefined) {
+		      if(!options.includePath && matchstring.length === 0) {
+			  matchstring += match[current].trim();
+		      } else {
+			  matchstring += ("," + match[current].trim());			
+		      }
+                  }
+              });
+              matches = matches + matchstring + "\n";
+              matchstring = undefined;
+              match = re.exec(filestring);
+          }
+	  if(options.printMatches) grunt.log.writeln(matches);
+          return matches;
       }).join("");
-
+        
       // Write the destination file.
       grunt.file.write(file.dest, src);
 
       // Print a success message.
-      grunt.log.writeln('File "' + file.dest + '" created.');
+      grunt.log.writeln('File "' + file.dest + '" created with '+countMatches+' matches.');
     });
   });
 
